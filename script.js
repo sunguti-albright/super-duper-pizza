d52528eadda4257b7f8dcd58c6e8bd7ae6ef9852
albright.human <sungutialbright@gmail.com>
2022-03-21 06:15:06 +0300

    add callback function to add another pizza

--- script.js
+++ script.js
@@ -46,28 +46,28 @@ $('#order').click (function(){
 });
 
 // //add another
-// $('#add-pizza').click (function(){
+$('#add-pizza').click (function(){
   
-//   var pizzaSize = $("#size option:selected").val();
-//   var pizzaToppings = $("#toppings option:selected").val();
-//   var pizzaCrust = $("#crust option:selected").val();
-//   var total = parseInt(pizzaSize) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
-//   order = order + 1;
-//   grandTotal = grandTotal + total;
+  var pizzaSize = $("#size option:selected").val();
+  var pizzaToppings = $("#toppings option:selected").val();
+  var pizzaCrust = $("#crust option:selected").val();
+  var total = parseInt(pizzaSize) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
+  order = order + 1;
+  grandTotal = grandTotal + total;
 
 
-//        var newPizza = new Pizza(pizzaSize, pizzaToppings, pizzaCrust, total);
+       var newPizza = new Pizza(pizzaSize, pizzaToppings, pizzaCrust, total);
   
-//       var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $(".size option:selected").text() + " - " + newPizza.size + '</td><td id="toppings">' + $(".toppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="crust">' + $(".crust option:selected").text() + " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
+      var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $(".size option:selected").text() + " - " + newPizza.size + '</td><td id="toppings">' + $(".toppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="crust">' + $(".crust option:selected").text() + " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
   
-//        $("#pizza").append(newRow);
+       $("#pizza").append(newRow);
 
 
-//   $("#order").hide();
-//   $('span').append(grandTotal);
+  $("#order").hide();
+  $('span').append(grandTotal);
 
-//   summary.show();
-//   totals.show();
+  summary.show();
+  totals.show();
            
 // });
 
